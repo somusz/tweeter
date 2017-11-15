621a352982ea90cbafb6bde20421845d7991e8a1
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-11-15 21:35:02 +0000

    error messages added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,68 +1,5 @@
 $(document).ready (function () {
 
-  var DATABASE = [
-{
-"user": {
-"name": "Johann von Goethe",
-"avatars": {
-"small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-"regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-"large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-},
-"handle": "@johann49"
-},
-"content": {
-"text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-},
-"created_at": 1461113796368
-},
-{
-"user": {
-"name": "Descartes",
-"avatars": {
-"small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-"regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-"large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-},
-"handle": "@rd"
-},
-"content": {
-"text": "Je pense , donc je suis"
-},
-"created_at": 1461113959088
-},
-{
-"user": {
-"name": "Newton",
-"avatars": {
-"small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-"regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-"large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-},
-"handle": "@SirIsaac"
-},
-"content": {
-"text": "If I have seen further it is by standing on the shoulders of giants"
-},
-"created_at": 1461116232227
-},
-{
-"user": {
-"name": "Mildred Oliver",
-"handle": "@DrOliver14",
-"avatars": {
-"small": "https://vanillicon.com/01cce0acc2f11f9d1a90f962bee1a9bf_50.png",
-"regular": "https://vanillicon.com/01cce0acc2f11f9d1a90f962bee1a9bf.png",
-"large": "https://vanillicon.com/01cce0acc2f11f9d1a90f962bee1a9bf_200.png"
-}
-},
-"content": {
-"text": "gjghj"
-},
-"created_at": 1510687886954
-}];
-
-
   let createTweetElement = function (tweet) {
     function escape(str) {
       var div = document.createElement('div');
@@ -112,8 +49,6 @@ $(document).ready (function () {
 
   }()
 
-  // renderTweets(DATABASE);
-
 
   // let createTweetElement = function (tweet) {
   //   let $tweet = $('<article>').addClass('tweet');
@@ -127,15 +62,6 @@ $(document).ready (function () {
   //   console.log($header);
   //   return $tweet;
   // }
-// console.log(createTweetElement());
-
-  // let createNewTweet = function (tweet) {
-  //   let tweetElements = tweet.map(createNewTweetElement);
-  //   return tweetElements;
-  // }
-
-  // let $container = $('#tweet-container');
-  // $container.append(createNewTweet(DATABASE));
 
 
 
--- public/scripts/upload-tweets.js
+++ public/scripts/upload-tweets.js
@@ -1,12 +1,25 @@
 $(document).ready(function () {
   $('.new-tweet form').submit (function () {
     event.preventDefault();
-    let tweetText = $(this).serialize();
-    $.ajax({
-      url: '/tweets/',
-      method: 'POST',
-      data: tweetText,
-      success: console.log('ok')
-    })
+    let tweetData = $(this).serialize();
+    let tweetText = $(this).children('textarea').val();
+    if (!tweetText) {
+      $('.new-tweet').append(`<div id=\'error-nomsg\'>Hum louder!</div>`);
+      setTimeout( function() {
+        $('#error-nomsg').remove();
+      }, 3000);
+    } else if (tweetText.length > 140) {
+      $('.new-tweet').append(`<div id=\'error-over140\'>Cut it out, you\'re over 140!</div>`);
+      setTimeout( function() {
+        $('#error-over140').remove();
+      }, 3000);
+    } else {
+      $.ajax({
+        url: '/tweets/',
+        method: 'POST',
+        data: tweetData,
+        success: console.log('ok')
+      })
+    }
   })
 });
