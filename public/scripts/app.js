9146d4cc35863f48a144ddc1fcce0164bb69ca02
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-11-15 01:41:49 +0000

    hard coded tweet articles are designed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,42 +5,42 @@
 //  */
 // $(document).ready (function () {
 
-// var DATABASE = ["EXAMPLE", "MORE"];
-// {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-// // };
+var DATABASE = ["EXAMPLE", "MORE"];
+{
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+// };
 
 
 
-//   let createNewTweetElement = function (tweet) {
-//     let $article = '<article> <header>;
-//     let $header = $('<header>');
-//     let $footer = $('<footer>');
-//     $article.append($header);
-//     // $article.addClass();
-//     $article.html(tweet);
-//     return $article;
-//   }
+  let createTweetElement = function (tweet) {
+    let $article = '<article> <header>';
+    let $header = $('<header>');
+    let $footer = $('<footer>');
+    $article.append($header);
+    // $article.addClass();
+    $article.html(tweet);
+    return $article;
+  }
 
-//   let createNewTweet = function (tweet) {
-//     let tweetElements = tweet.map(createNewTweetElement);
-//     return tweetElements;
-//   }
+  let createNewTweet = function (tweet) {
+    let tweetElements = tweet.map(createNewTweetElement);
+    return tweetElements;
+  }
 
-//   let $container = $('#tweet-container');
-//   $container.append(createNewTweet(DATABASE));
+  let $container = $('#tweet-container');
+  $container.append(createNewTweet(DATABASE));
 
-// })
+})
 
