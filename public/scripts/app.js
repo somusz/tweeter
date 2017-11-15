ac10f811f457f1cec8456f692a528b4f5c00a440
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-11-15 23:06:07 +0000

    compose button and css added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -33,8 +33,8 @@ $(document).ready (function () {
 
   let renderTweets = function (tweets) {
     tweets.forEach( function (tweet) {
-      let toAppend = createTweetElement(tweet);
-      $('#tweet-container').append(toAppend);
+      let toAdd = createTweetElement(tweet);
+      $('#tweet-container').prepend(toAdd);
     })
   }
 
@@ -46,8 +46,33 @@ $(document).ready (function () {
         renderTweets(data);
       }
     })
+  }
+  loadTweets();
 
-  }()
+  $('.new-tweet form').submit (function () {
+    event.preventDefault();
+    let tweetData = $(this).serialize();
+    let tweetText = $(this).children('textarea').val();
+    if (!tweetText) {
+      $('.new-tweet').append(`<div id=\'error-nomsg\'>Hum louder!</div>`);
+      setTimeout( function() {
+        $('#error-nomsg').remove();
+      }, 3000);
+    } else if (tweetText.length > 140) {
+      $('.new-tweet').append(`<div id=\'error-over140\'>Cut it out, you\'re over 140!</div>`);
+      setTimeout( function() {
+        $('#error-over140').remove();
+      }, 3000);
+    } else {
+      $.ajax({
+        url: '/tweets/',
+        method: 'POST',
+        data: tweetData
+      })
+      loadTweets();
+      $('#new-tweet-input').val('');
+    }
+  })
 
 
   // let createTweetElement = function (tweet) {
