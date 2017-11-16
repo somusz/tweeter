3d92faa2f314db4d6f1d6db2e403db616d278339
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-11-16 00:49:55 +0000

    minor edits after code review

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,32 @@
 $(document).ready (function () {
 
-  let createTweetElement = function (tweet) {
-    function escape(str) {
+  let escape = function (str) {
       var div = document.createElement('div');
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
     }
-    function msToDays (ms) {
-      let date = new Date(ms);
-      let now = new Date(Date.now());
-      return now.getDay() - date.getDay();
-    }
 
+  let msToDays = function (ms) {
+    let date = new Date(ms);
+    let now = new Date(Date.now());
+    return now.getDay() - date.getDay();
+  }
+
+  let createTweetElement = function (tweet) {
     return $(
       `<article>
         <header>
-          <img class=\'hovering\' src=${tweet.user.avatars.small}>
-          <span class=\'user-name hovering\'>${tweet.user.name}</span>
-          <span class=\'handle hovering\'>${tweet.user.handle}</span>
+          <img class="hovering" src=${tweet.user.avatars.small}>
+          <span class="user-name hovering">${tweet.user.name}</span>
+          <span class="handle hovering">${tweet.user.handle}</span>
         </header>
-      <p>${escape(tweet.content.text)}</p>
+        <p>${escape(tweet.content.text)}</p>
         <footer>
           <span>Posted ${msToDays(tweet.created_at)} days ago</span>
-          <span class=\'links\'>
-            <a href=\'\'><i class=\'fa fa-flag\' aria-hidden=\'true\'></i></a>
-            <a href=\'\'><i class=\'fa fa-retweet\' aria-hidden=\'true\'></i></a>
-            <a href=\'\'><i class=\'fa fa-heart\' aria-hidden=\'true\'></i></a>
+          <span class="links">
+            <a href="><i class="fa fa-flag" aria-hidden="true"></i></a>
+            <a href="><i class="fa fa-retweet" aria-hidden="true"></i></a>
+            <a href="><i class="fa fa-heart" aria-hidden="true"></i></a>
           </span>
         </footer>
       </article>`);
@@ -35,7 +36,7 @@ $(document).ready (function () {
     tweets.forEach( function (tweet) {
       let toAdd = createTweetElement(tweet);
       $('#tweet-container').prepend(toAdd);
-    })
+    });
   }
 
   let loadTweets = function () {
@@ -45,31 +46,42 @@ $(document).ready (function () {
       success: function (data) {
         renderTweets(data);
       }
-    })
+    });
   }
+
   loadTweets();
 
   $('.new-tweet form').submit (function () {
     event.preventDefault();
+
     let tweetData = $(this).serialize();
     let tweetText = $(this).children('textarea').val();
+
     if (!tweetText) {
-      $('.new-tweet').append(`<div id=\'error-nomsg\'>Hum louder!</div>`);
+      $('.new-tweet').append(`<div id="error-nomsg">Hum louder!</div>`);
+
       setTimeout( function() {
         $('#error-nomsg').remove();
       }, 3000);
-    } else if (tweetText.length > 140) {
-      $('.new-tweet').append(`<div id=\'error-over140\'>Cut it out, you\'re over 140!</div>`);
+    }
+
+    else if (tweetText.length > 140) {
+      $('.new-tweet').append(`<div id="error-over140">Cut it out, you're over 140!</div>`);
+
       setTimeout( function() {
         $('#error-over140').remove();
       }, 3000);
-    } else {
+    }
+
+    else {
       $.ajax({
         url: '/tweets/',
         method: 'POST',
         data: tweetData
-      })
+      });
+
       loadTweets();
+
       $('#new-tweet-input').val('');
     }
   })
