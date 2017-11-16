6ebd70687676093c8a0f4ae4fc231120d1f349a3
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-11-16 20:18:02 +0000

    mongo connection established, get working

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
+// const simulateDelay = require("./util/simulate-delay");
 
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
@@ -17,11 +17,31 @@ module.exports = function makeDataHelpers(db) {
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
+      // simulateDelay(() => {
+      db.collection("tweets").find().toArray((err, results) => {
+        if (err) {
+          callback(err);
+        }
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+        callback(null, results.sort(sortNewestFirst));
       });
     }
 
+
+
+
+      // db.collection("tweets").find().toArray((err, tweets) => {
+        // if (err) {
+          // return callback(err);
+        // }
+        // console.log(tweets);
+
+        // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+        // callback(null, db.tweets.sort(sortNewestFirst));
+        // callback(null, tweets);
+
+      // });
+
+      // });
   };
 }
